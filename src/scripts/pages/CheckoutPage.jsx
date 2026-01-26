84d9d49aab894b478439b459a059032ddd534fea
hackden3618 <denniswambugu@protonmail.com>
2026-01-26 21:43:19 +0300

    here we go

--- src/scripts/pages/CheckoutPage.jsx
+++ src/scripts/pages/CheckoutPage.jsx
@@ -5,15 +5,18 @@ export function CheckoutPage() {
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
-            <a href="index.html">
+            <a href="/">
               <img className="logo" src="images/logo.png" />
               <img className="mobile-logo" src="images/mobile-logo.png" />
             </a>
           </div>
 
           <div className="checkout-header-middle-section">
-            Checkout (<a className="return-to-home-link"
-              href="index.html">3 items</a>)
+            Checkout (
+            <a className="return-to-home-link" href="/">
+              3 items
+            </a>
+            )
           </div>
 
           <div className="checkout-header-right-section">
@@ -33,16 +36,16 @@ export function CheckoutPage() {
               </div>
 
               <div className="cart-item-details-grid">
-                <img className="product-image"
-                  src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                <img
+                  className="product-image"
+                  src="images/products/athletic-cotton-socks-6-pairs.jpg"
+                />
 
                 <div className="cart-item-details">
                   <div className="product-name">
                     Black and Gray Athletic Cotton Socks - 6 Pairs
                   </div>
-                  <div className="product-price">
-                    $10.90
-                  </div>
+                  <div className="product-price">$10.90</div>
                   <div className="product-quantity">
                     <span>
                       Quantity: <span className="quantity-label">2</span>
@@ -61,22 +64,25 @@ export function CheckoutPage() {
                     Choose a delivery option:
                   </div>
                   <div className="delivery-option">
-                    <input type="radio" checked
+                    <input
+                      type="radio"
+                      checked
                       className="delivery-option-input"
-                      name="delivery-option-1" />
+                      name="delivery-option-1"
+                    />
                     <div>
                       <div className="delivery-option-date">
                         Tuesday, June 21
                       </div>
-                      <div className="delivery-option-price">
-                        FREE Shipping
-                      </div>
+                      <div className="delivery-option-price">FREE Shipping</div>
                     </div>
                   </div>
                   <div className="delivery-option">
-                    <input type="radio"
+                    <input
+                      type="radio"
                       className="delivery-option-input"
-                      name="delivery-option-1" />
+                      name="delivery-option-1"
+                    />
                     <div>
                       <div className="delivery-option-date">
                         Wednesday, June 15
@@ -87,9 +93,11 @@ export function CheckoutPage() {
                     </div>
                   </div>
                   <div className="delivery-option">
-                    <input type="radio"
+                    <input
+                      type="radio"
                       className="delivery-option-input"
-                      name="delivery-option-1" />
+                      name="delivery-option-1"
+                    />
                     <div>
                       <div className="delivery-option-date">
                         Monday, June 13
@@ -109,16 +117,16 @@ export function CheckoutPage() {
               </div>
 
               <div className="cart-item-details-grid">
-                <img className="product-image"
-                  src="images/products/intermediate-composite-basketball.jpg" />
+                <img
+                  className="product-image"
+                  src="images/products/intermediate-composite-basketball.jpg"
+                />
 
                 <div className="cart-item-details">
                   <div className="product-name">
                     Intermediate Size Basketball
                   </div>
-                  <div className="product-price">
-                    $20.95
-                  </div>
+                  <div className="product-price">$20.95</div>
                   <div className="product-quantity">
                     <span>
                       Quantity: <span className="quantity-label">1</span>
@@ -138,20 +146,25 @@ export function CheckoutPage() {
                   </div>
 
                   <div className="delivery-option">
-                    <input type="radio" className="delivery-option-input"
-                      name="delivery-option-2" />
+                    <input
+                      type="radio"
+                      className="delivery-option-input"
+                      name="delivery-option-2"
+                    />
                     <div>
                       <div className="delivery-option-date">
                         Tuesday, June 21
                       </div>
-                      <div className="delivery-option-price">
-                        FREE Shipping
-                      </div>
+                      <div className="delivery-option-price">FREE Shipping</div>
                     </div>
                   </div>
                   <div className="delivery-option">
-                    <input type="radio" checked className="delivery-option-input"
-                      name="delivery-option-2" />
+                    <input
+                      type="radio"
+                      checked
+                      className="delivery-option-input"
+                      name="delivery-option-2"
+                    />
                     <div>
                       <div className="delivery-option-date">
                         Wednesday, June 15
@@ -162,8 +175,11 @@ export function CheckoutPage() {
                     </div>
                   </div>
                   <div className="delivery-option">
-                    <input type="radio" className="delivery-option-input"
-                      name="delivery-option-2" />
+                    <input
+                      type="radio"
+                      className="delivery-option-input"
+                      name="delivery-option-2"
+                    />
                     <div>
                       <div className="delivery-option-date">
                         Monday, June 13
@@ -179,9 +195,7 @@ export function CheckoutPage() {
           </div>
 
           <div className="payment-summary">
-            <div className="payment-summary-title">
-              Payment Summary
-            </div>
+            <div className="payment-summary-title">Payment Summary</div>
 
             <div className="payment-summary-row">
               <div>Items (3):</div>
@@ -214,7 +228,6 @@ export function CheckoutPage() {
           </div>
         </div>
       </div>
-
     </>
   );
 }
